15f4e311de031b8d9cedb504bde9f0455345ba60
thiagomiyaguchi <89756000+thiagomiyaguchi@users.noreply.github.com>
2024-09-25 23:54:54 -0300

    comentarios

--- stem/src/components/pages/Projeto1.jsx
+++ stem/src/components/pages/Projeto1.jsx
@@ -1,31 +1,32 @@
-import { useEffect, useState } from "react";
-import "./projeto1.scss";
-import axios from "axios";
-import Ex from "./../../assets/insta.png";
+import { useEffect, useState } from 'react';
+import './projeto1.scss';
+import axios from 'axios';
+import Ex from './../../assets/insta.png';
+import Form from '../form/Form';
 
 const Projeto1 = () => {
   const [comentarios, setComentarios] = useState([]);
-
+  const getComentarios = async () => {
+    try {
+      const response = await axios.get(
+        'http://82.112.244.100:8070/comentarios'
+      );
+      console.log(response.data);
+      setComentarios(
+        response.data.filter((item) => item.projeto === 'projeto1')
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
-    const getComentarios = async () => {
-      try {
-        const response = await axios.get(
-          "http://82.112.244.100:8070/comentarios"
-        );
-        console.log(response.data);
-        setComentarios(
-          response.data.filter((item) => item.projeto === "projeto1")
-        );
-      } catch (error) {
-        console.log(error);
-      }
-    };
     getComentarios();
   }, []);
+
   return (
-    <div className="projeto1">
+    <div className='projeto1'>
       <h1>Projeto 1</h1>
-      <p className="desc">
+      <p className='desc'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam mattis
         pharetra purus ac venenatis. Orci varius natoque penatibus et magnis dis
         parturient montes, nascetur ridiculus mus. Donec posuere, lectus sed
@@ -39,23 +40,24 @@ const Projeto1 = () => {
         interdum.
       </p>
 
-      <div className="im">
-        <img src={Ex} alt="imagem do projeto1" />
-        <img src={Ex} alt="imagem do projeto1" />
-        <img src={Ex} alt="imagem do projeto1" />
+      <div className='im'>
+        <img src={Ex} alt='imagem do projeto1' />
+        <img src={Ex} alt='imagem do projeto1' />
+        <img src={Ex} alt='imagem do projeto1' />
       </div>
       <h1>Feedbacks:</h1>
 
       {comentarios.map((comentario) => (
-        <div className="comnts" key={comentario.id}>
+        <div className='comnts' key={comentario.id}>
           <div>
-            <h2 className="tl">Nome:</h2>
-            <p className="tx">{comentario.nome}</p>
-            <h2 className="tl">Comentário:</h2>
+            <h2 className='tl'>Nome:</h2>
+            <p className='tx'>{comentario.nome}</p>
+            <h2 className='tl'>Comentário:</h2>
           </div>
-          <p className="tx">{comentario.comentario}</p>
+          <p className='tx'>{comentario.comentario}</p>
         </div>
       ))}
+      <Form projeto={'projeto1'} atualiza={getComentarios} />
     </div>
   );
 };
